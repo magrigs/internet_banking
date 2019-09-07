f6e3c7f73d2400b347003e544888a13e1b69363a
magrigs <eliandrebanzouzi@gmail.com>
2019-09-07 10:11:54 +0200

    connect ok
    is ok good

--- internet_banking_system/urls.py
+++ internet_banking_system/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url(r'^$', include('pages.urls')),
+    url(r'^$', include('users.urls')),
     url(r'^users/', include('users.urls')),
     url(r'^pages/', include('pages.urls')),
     url(r'^admin/', admin.site.urls),
