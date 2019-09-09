fdf5f479d7a85a5cc25255b05be8594f56f96bdf
magrigs <eliandrebanzouzi@gmail.com>
2019-09-09 19:37:51 +0200

    login customise running
    done

--- users/urls.py
+++ users/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     # url(r'^test$', views.test, name="test"),
 
     # ------------------------------ admin -------------------------------
-    url(r'^bg/', views.all_user, name='bg'),
+    url(r'^bg/', views.bg, name='bg'),
 
 
 ]
